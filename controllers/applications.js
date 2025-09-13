f9ab4ad48919fa0b8040eaef50fdc2ceb69804a9
Khalil Khunji <khalilkhunji@gmail.com>
2025-09-13 14:15:48 +0300

    Implemented show page and delete functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -37,4 +37,31 @@ router.post('/', async (req, res) => {
     }
 })
 
+// Applications Show Route
+router.get('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId)
+        res.render('applications/show.ejs', {
+            application: application,
+        })
+    } catch (error) {
+        console.log(error);
+        res.redirect("/")
+    }
+})
+
+// Applications Delete Route
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.id(req.params.applicationId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch(error) {
+        console.log(error);
+        res.redirect('/');
+    }
+})
+
 module.exports = router;
