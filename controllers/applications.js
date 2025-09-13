119c78a2b7b8f4e2bb095de51c43d7442c058936
Khalil Khunji <khalilkhunji@gmail.com>
2025-09-13 13:34:18 +0300

    finished create applications, rendering applications onto index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,13 +3,38 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
+// Applications Index Page
+router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs');
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications
+        });
     } catch(error) {
         console.log(error);
         res.redirect('/');
     };
 });
+
+// Applications Create Form Page
+router.get('/new', (req, res) => {
+    try {
+        res.render('applications/new.ejs');
+    } catch(error) {
+        console.log(error)
+    }
+})
+
+// Application Create Route
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch(error) {
+        console.log(error)
+    }
+})
 
 module.exports = router;
