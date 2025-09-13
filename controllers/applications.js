52758302f00e643ad093edf4d6e07319ba6d823e
Khalil Khunji <khalilkhunji@gmail.com>
2025-09-13 14:33:21 +0300

    completed CRUD

--- controllers/applications.js
+++ controllers/applications.js
@@ -64,4 +64,32 @@ router.delete('/:applicationId', async (req, res) => {
     }
 })
 
+// Applications Edit Form Route
+router.get('/:applicationId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        res.render('applications/edit.ejs', {
+            application: application,
+        });
+    } catch(error) {
+        console.log(error);
+        res.redirect("/");
+    }
+})
+
+// Applications Update Route
+router.put('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        application.set(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications/${req.params.applicationId}`)
+    } catch(error) {
+        console.log(error)
+        res.redirect("/")
+    }
+})
+
 module.exports = router;
